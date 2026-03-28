342ddfa3eae1c4afcc5fbaa9804ab31b558d248c
ebrahim-code <ebrahimworkie@gmail.com>
2026-03-28 15:10:54 +0300

    Fix: correct middleware import in coupons route

--- routes/coupons.js
+++ routes/coupons.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { createCoupon, getCoupons, validateCoupon, deleteCoupon } = require("../controllers/couponController");
-const { protect } = require("../middleware/authMiddleware");
+const protect = require("../middleware/auth");
 
 router.post("/", protect, createCoupon);
 router.get("/", protect, getCoupons);
